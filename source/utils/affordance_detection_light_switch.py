7a7ab0553cbc803bb4c847ce3c7cd325f41ab9d4
timengelbracht <tim@hbs16.de>
2024-05-29 14:12:41 +0200

    moved gpt api query into own function GPT4_query, implemented advanced_affordance

--- source/utils/affordance_detection_light_switch.py
+++ source/utils/affordance_detection_light_switch.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from PIL import Image
 from typing import Dict
 
-def compute_affordance_VLM_GPT4(cropped_image: np.ndarray, affordance_classes, api_key):
+def compute_affordance_VLM_GPT4(cropped_image: np.ndarray, affordance_classes, api_key) -> int:
     """
     Takes in the cropped image of a light switch (bounding box) and predicts the affordance using VLM GPT4
 
@@ -27,6 +27,23 @@ def compute_affordance_VLM_GPT4(cropped_image: np.ndarray, affordance_classes, a
         else:
             prmpt += f"{value} which is unlikely (if yes answer {key})."
 
+    response = GPT4_query(api_key, prmpt, cropped_image, max_tokens=1, detail="low")
+
+    if response.status_code == 200:
+        affordance_key = int(response.json()["choices"][0]["message"]["content"])
+        return affordance_key
+    else:
+        print("Error:", response.status_code)
+
+def GPT4_query(api_key, prompt, image, max_tokens: int = 1, detail: str="low"):
+    """
+    send query to GPT4 model via api
+
+    @param api_key:
+    @param prompt:
+    @param image:
+    @return:
+    """
     # resize image
     resized_image = cv2.resize(cropped_image, (512, 512))
 
@@ -51,30 +68,130 @@ def compute_affordance_VLM_GPT4(cropped_image: np.ndarray, affordance_classes, a
                 "content": [
                     {
                         "type": "text",
-                        "text": prmpt
+                        "text": prompt
                     },
                     {
                         "type": "image_url",
                         "image_url": {
                             "url": f"data:image/jpeg;base64,{base64_image}",
-                            "detail": "low"
+                            "detail": detail
                         }
                     }
                 ]
             }
         ],
-        "max_tokens": 1
+        "max_tokens": max_tokens
     }
 
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
+    return response
+
+def compute_advanced_affordance_VLM_GPT4(cropped_image: np.ndarray, affordance_dict: Dict, api_key: str) -> Dict:
+    """
+    Takes in the cropped image of a light switch (bounding box) and predicts advanced affordance using VLM GPT4, returning a dict
+    @param cropped_image:
+    @param affordance_classes:
+    @param api_key:
+    @return:
+    """
+
+    # build prompt
+    prompt = "Describe light switch: "
+    for idx, (key, value) in enumerate(affordance_dict.items()):
+        prompt += f"{key} ({'/'.join(value)})"
+        if idx < len(affordance_dict) - 1:
+            prompt += ", "
+        else:
+            prompt += ". "
+    prompt += f"Format: <{'>, <'.join(list(affordance_dict.keys()))}>"
+
+    response = GPT4_query(api_key, prompt, cropped_image, max_tokens=50, detail="low")
+
     if response.status_code == 200:
-        affordance_key = int(response.json()["choices"][0]["message"]["content"])
+        values = response.json()["choices"][0]["message"]["content"].split(", ")
+        keys = list(affordance_dict.keys())
+        affordance_dict = dict(zip(keys, values))
+        return affordance_dict
     else:
         print("Error:", response.status_code)
 
-    return affordance_key
 
+def GPT4_query(api_key, prompt, cropped_image, max_tokens: int = 1, detail: str="low"):
+    """
+    send query to GPT4 model via api
+
+    @param api_key:
+    @param prompt:
+    @param image:
+    @return:
+    """
+    # resize image
+    resized_image = cv2.resize(cropped_image, (512, 512))
+
+    # encode image
+    image = Image.fromarray(resized_image)
+    buffer = BytesIO()
+    image.save(buffer, format="PNG")
+    buffer.seek(0)
+    image_bytes = buffer.read()
+    base64_image = base64.b64encode(image_bytes).decode('utf-8')
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}",
+                            "detail": detail
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": max_tokens
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    return response
+
+def compute_affordance_VLM_GPT4_test(cropped_image: np.ndarray, affordance_classes: Dict, api_key: str):
+
+    # prompt = "Tell me what you see, how to interact with it, orientation, geometry. USE KEYWORDS No Sentences"
+    # prompt = "Describe Light switch type (single/double, push, rotating, rocker), interaction, geometry/alignment. USE KEYWORDS NO SETENCES"
+    # prompt = ("Describe light switch: button count (single/double/multi), switch type: (push button/rocker switch/rotating switch), button stacking (vetical/horizontal/None if single). Format: <button count>, <switch type>, <button stacking>")
+
+    affordance_dict = {"switch type": ["push button switch", "rocker switch", "rotating switch"],
+                       "button count": ["single", "double", "multi"],
+                       "button stacking": ["vertical", "horizontal", "none"]}
+
+    # build prompt
+    prompt = "Describe light switch: "
+    for idx, (key, value) in enumerate(affordance_dict.items()):
+        prompt += f"{key} ({'/'.join(value)})"
+        if idx < len(affordance_dict) - 1:
+            prompt += ", "
+        else:
+            prompt += ". "
+    prompt += f"Format: <{'>, <'.join(list(affordance_dict.keys()))}>"
+
+    response = GPT4_query(api_key, prompt, cropped_image, max_tokens=50, detail="low")
+
+    a = 2
 
 def compute_affordance_VLM_llava(cropped_image, affordance_classes):
 
@@ -102,24 +219,33 @@ def compute_affordance_VLM_llava(cropped_image, affordance_classes):
     torch.cuda.empty_cache()
 
     return affordance_key
-def test_compute_affordance_VLM(model: str, affordance_classes: Dict[int, str], cropped_image: np.ndarray):
+def compute_affordance_VLM_test(model: str, affordance_classes: Dict[int, str], cropped_image: np.ndarray):
 
     if model=="GPT4":
         api_key = "..."
         affordance_key = compute_affordance_VLM_GPT4(cropped_image=cropped_image, affordance_classes=affordance_classes, api_key=api_key)
+        return affordance_key
     if model == "LLAVA":
         affordance_key = compute_affordance_VLM_llava(cropped_image=cropped_image, affordance_classes=affordance_classes)
+        return affordance_key
     else:
         print("Model not supported")
 
-    return affordance_key
+
 
 if __name__ == "__main__":
-    affordance_classes = {0: "SINGLE PUSH",
-                          1: "DOUBLE PUSH",
-                          2: "ROTATING",
-                          3: "something else"}
+    # affordance_classes = {0: "SINGLE PUSH",
+    #                       1: "DOUBLE PUSH",
+    #                       2: "ROTATING",
+    #                       3: "something else"}
+    # cropped_image = cv2.imread("/home/cvg-robotics/tim_ws/turn_button_spot.png")
+    # affordance_key = compute_affordance_VLM_test(model="GPT4", affordance_classes=affordance_classes, cropped_image=cropped_image)
+    affordance_dict = {"switch type": ["push button switch", "rocker switch", "rotating switch", "none"],
+                       "button count": ["single", "double", "multi", "none"],
+                       "button stacking": ["vertical", "horizontal", "none"]}
 
-    cropped_image = cv2.imread("/home/cvg-robotics/tim_ws/turn_button_spot.png")
+    cropped_image = cv2.imread("/home/cvg-robotics/tim_ws/GPT4_visuals/power_plug.png")
+    api_key = "..."
 
-    test_compute_affordance_VLM(model="LLAVA", affordance_classes=affordance_classes, cropped_image=cropped_image)
+    compute_advanced_affordance_VLM_GPT4(cropped_image=cropped_image, affordance_dict=affordance_dict, api_key=api_key)
+    a = 2
