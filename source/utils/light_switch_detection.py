9c47e0ba46d39a64c6158985043492d05d09fb37
timengelbracht <tim@hbs16.de>
2024-10-20 18:38:23 +0200

    clean up light switch detection

--- source/utils/light_switch_detection.py
+++ source/utils/light_switch_detection.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2
 import supervision as sv
 from inference.models.yolo_world.yolo_world import YOLOWorld
-from ultralytics import YOLO
+from ultralytics import YOLO, YOLOWorld
+from ultralytics.data.dataset import YOLODataset
 import cv2
 import matplotlib.pyplot as plt
 from source.utils.object_detetion import BBox
@@ -27,7 +28,6 @@ def filter_detections_ultralytics(detections, filter_squaredness=True, filter_ar
 
     detections = detections[0].cpu()
     xyxy = detections.boxes.xyxy.numpy()
-    indices = np.linspace(0, xyxy.shape[0] - 1, xyxy.shape[0])
 
     # filter squaredness outliers
     if filter_squaredness:
@@ -39,23 +39,11 @@ def filter_detections_ultralytics(detections, filter_squaredness=True, filter_ar
         keep_1 = squaredness > 0.5
         xyxy = xyxy[keep_1, :]
 
-        # xyxy = xyxy[keep_1, :]
-        # indices = indices[keep_1]
-
-    # squaredness = (np.minimum(xyxy[:, 2] - xyxy[:, 0],
-    #                           xyxy[:, 3] - xyxy[:, 1]) /
-    #                np.maximum(xyxy[:, 2] - xyxy[:, 0],
-    #                           xyxy[:, 3] - xyxy[:, 1]))
-
     #filter area outliers
     if filter_area:
         areas = (xyxy[:, 2] - xyxy[:, 0]) * (xyxy[:, 3] - xyxy[:, 1])
         keep_2 = areas < 3*np.median(areas)
         xyxy = xyxy[keep_2, :]
-        # keep_2 = np.logical_and(areas < 3*np.median(areas), squaredness > 0.1)
-
-    # areas = (xyxy[:, 2] - xyxy[:, 0]) * (xyxy[:, 3] - xyxy[:, 1])
-    # keep_1 = np.logical_and(areas < 3*np.median(areas), squaredness > 0.1)#0.88
 
     # filter bounding boxes within larger ones
     if filter_within:
@@ -70,54 +58,23 @@ def filter_detections_ultralytics(detections, filter_squaredness=True, filter_ar
         keep_3[idx_remove] = False
         xyxy = xyxy[keep_3, :]
 
-    # merged_keep = np.logical_and(keep_1, keep_2)
-
-    # bbox = xyxy[merged_keep,:]
-    # bbox = xyxy[keep_2, :]
     bbox = xyxy
     return bbox
 
 
-def predict_light_switches(image: np.ndarray, model_type: str = "yolov9c", vis_block: bool = False):
+def predict_light_switches(image: np.ndarray, model_type: str = "yolov8", vis_block: bool = False):
 
-    if model_type == "yolo_world/l":
-        classes = ["round light switch",
-                   "white light switch",
-                   "small light switch",
-                   "push button light switch",
-                   "rotating light switch",
-                   "turning round  light switch",
-                   "double push button light switch",
-                   "black light switch"]
+    if model_type == "yolo_world":
+        model = YOLOWorld("yolov8s-world.pt")
+        model.set_classes(["light switch"])
 
-        model = YOLOWorld(model_id=model_type)
-        model.set_classes(classes)
+        results_predict = model.predict(image)
+        results_predict[0].show()
 
-        def _callback(image_slice: np.ndarray) -> sv.Detections:
-            result = model.infer(image_slice, confidence=0.005)
-            return sv.Detections.from_inference(result).with_nms(threshold=0.05, class_agnostic=True)
-
-        slicer = sv.InferenceSlicer(callback=_callback, slice_wh=(image.shape[0] // 1, image.shape[1] // 1),
-                                    overlap_ratio_wh=(0.2, 0.2))
-
-        detections = slicer(image)
-        detections = _filter_detections_YOLOWorld(detections=detections)
-
-        BOUNDING_BOX_ANNOTATOR = sv.BoundingBoxAnnotator(thickness=2, color=sv.Color.RED)
-        LABEL_ANNOTATOR = sv.LabelAnnotator(text_thickness=1, text_scale=1, text_color=sv.Color.BLACK)
-
-        if vis_block:
-            annotated_image = image.copy()
-            annotated_image = BOUNDING_BOX_ANNOTATOR.annotate(annotated_image, detections)
-            # annotated_image = LABEL_ANNOTATOR.annotate(annotated_image, detections)
-            sv.plot_image(annotated_image, (20, 20))
-
-        return detections
-
-    elif model_type == "yolov9c":
-        model = YOLO('/home/cvg-robotics/tim_ws/YOLO-World/runs/detect/train27/weights/best.pt')#12, 27
+    elif model_type == "yolov8":
+        model = YOLO('../../weights/train27/weights/best.pt')
         results_predict = model.predict(source=image, imgsz=1280, conf=0.15, iou=0.4, max_det=9, agnostic_nms=True,
-                                        save=False)  # save plotted images 0.3
+                                        save=False)
 
         boxes = filter_detections_ultralytics(detections=results_predict)
 
@@ -131,17 +88,36 @@ def predict_light_switches(image: np.ndarray, model_type: str = "yolov9c", vis_b
 
                 cv2.rectangle(canv, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
-            bbs = []
-            for box in boxes:
-                bbs.append(BBox(box[0], box[1], box[2], box[3]))
-
             plt.imshow(cv2.cvtColor(canv, cv2.COLOR_BGR2RGB))
             plt.show()
 
+        bbs = []
+        for box in boxes:
+            bbs.append(BBox(box[0], box[1], box[2], box[3]))
+
         return bbs
 
-if __name__ == "__main__":
-    image = cv2.imread("/home/cvg-robotics/tim_ws/IMG_1012.jpeg")
-    model_type = "yolov9c"
-    detections = predict_light_switches(image, model_type, vis_block=True)
+def validate_light_switches(data_path, model_type: str = "yolov8"):
+
+    if model_type == "yolov8":
+        model = YOLO('../../weights/train27/weights/best.pt')
+        metrics = model.val(data=data_path, imgsz=1280)
+        result = metrics.results_dict
+        a = 2
+
+    elif model_type == "yolo_world":
+        model = YOLOWorld("yolov8s-world.pt")
+        model.set_classes(["light switch"])
+        metrics = model.val(data=data_path, imgsz=1280)
+        result = metrics.results_dict
+        a = 2
+
     a = 2
+
+    pass
+
+
+if __name__ == "__main__":
+
+    data_path = "/home/cvg-robotics/tim_ws/data_switch_detection/20240706_test/data.yaml"
+    validate_light_switches(data_path=data_path, model_type="yolov8")
